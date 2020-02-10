f0b0105dfbc67fe9f2ca216383a648f4214fc0cd
kobakakzu0429 <kobakazu10@gmail.com>
2020-02-10 10:38:47 +0900

    add global style

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./components/App";
+import { GlobalStyle } from "./theme/GlobalStyle";
+import { App } from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
